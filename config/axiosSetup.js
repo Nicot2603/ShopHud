ec2f5c460c4a03f73f78a804edbbaff8cb63d7aa
Nicot2603 <Carol.nico2603@gmail.com>
2025-04-02 06:21:42 -0500

    Update axiosSetup.js

--- config/axiosSetup.js
+++ config/axiosSetup.js
@@ -1,2 +1,19 @@
-import axios from "axios";
-axios.defaults.baseURL = "https://9440-186-29-84-254.ngrok-free.app/api"; 
+const fetchVisits = async () => {
+  try {
+    const response = await fetch("https://9440-186-29-84-254.ngrok-free.app/api/admin/visitas", {
+      headers: {
+        "Accept": "application/json",
+        "ngrok-skip-browser-warning": "true"
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log("Datos obtenidos del servidor:", data);
+  } catch (err) {
+    console.error("Error al obtener visitas:", err);
+  }
+};
