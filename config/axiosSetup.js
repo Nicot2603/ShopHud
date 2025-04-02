8d4f59c18067a9053c8e4eeb6ca5abae01271f46
Nicot2603 <Carol.nico2603@gmail.com>
2025-04-02 06:28:34 -0500

    Update axiosSetup.js

--- config/axiosSetup.js
+++ config/axiosSetup.js
@@ -1,23 +1,2 @@
-const fetchData = async (endpoint, options = {}) => {
-  try {
-    const response = await fetch(`https://9440-186-29-84-254.ngrok-free.app/api${endpoint}`, {
-      ...options,
-      headers: {
-        "Content-Type": "application/json",
-        "ngrok-skip-browser-warning": "true",
-        ...(options.headers || {})
-      }
-    });
-
-    if (!response.ok) {
-      throw new Error(`Error en la solicitud: ${response.status}`);
-    }
-
-    return await response.json();
-  } catch (error) {
-    console.error("Error en fetchData:", error);
-    throw error;
-  }
-};
-
-export default fetchData;
+import axios from "axios"; 
+axios.defaults.baseURL = "https://9440-186-29-84-254.ngrok-free.app/api";
