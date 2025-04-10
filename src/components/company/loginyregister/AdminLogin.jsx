8f18bbcb121c8651d8100f1d0e226802a68b1105
Nicot2603 <Carol.nico2603@gmail.com>
2025-04-10 08:09:35 -0500

    Update AdminLogin.jsx

--- src/components/company/loginyregister/AdminLogin.jsx
+++ src/components/company/loginyregister/AdminLogin.jsx
@@ -29,7 +29,7 @@ const AdminLogin = () => {
     try {
 
 
-      const response = await axios.post("/admin/login", formData);
+      const response = await axios.post("http://c37b-186-154-59-147.ngrok-free-app/api/admin/login", formData);
 
       if (response.data.token) {
         localStorage.setItem("adminToken", response.data.token);
