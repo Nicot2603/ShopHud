5ea69655975ca2d21f61723e836450e22d992463
Nicot2603 <carol.nico2603@gmail.com>
2025-04-10 17:45:06 -0500

    ahora si

--- config/axiosSetup.js
+++ config/axiosSetup.js
@@ -1,3 +1,3 @@
 import axios from "axios";
 
-axios.defaults.baseURL = " https://97d9-186-154-59-174.ngrok-free.app/api";
+axios.defaults.baseURL = " https://97d9-186-154-59-147.ngrok-free.app/api";
