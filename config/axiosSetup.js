9a76fbe0f945b60b1cceb01e65074877f778d8ee
Nicot2603 <Carol.nico2603@gmail.com>
2025-04-10 19:44:38 -0500

    Update axiosSetup.js

--- config/axiosSetup.js
+++ config/axiosSetup.js
@@ -1,3 +1,7 @@
 import axios from "axios";
 
 axios.defaults.baseURL = " https://97d9-186-154-59-147.ngrok-free.app/api";
+
+// Agregar headers globales
+axios.defaults.headers.common["Accept"] = "application/json";
+axios.defaults.headers.common["ngrok-skip-browser-warning"] = "true";
